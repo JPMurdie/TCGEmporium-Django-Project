6fd8a27fe33d8bd7c74e87ec23ca5e55a6212873
JPMurdie <jpmurdie@gmail.com>
2020-12-21 09:20:47 +0000

    Product Models completed and registered in products admin file

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Mtg_Cards, Mtg_Sets, Sales_Category
 
-# Register your models here.
+
+admin.site.register(Mtg_Cards)
+admin.site.register(Mtg_Sets)
+admin.site.register(Sales_Category)
--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Sales_Category(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
@@ -23,20 +24,23 @@ class Mtg_Sets(models.Model):
 
 
 class Mtg_Cards(models.Model):
-    sku = models.CharField(max_length=254, null=True, blank=True)
+    sku = models.CharField(max_length=254, blank=True)
     name = models.CharField(max_length=254)
-    lang = models.CharField(max_length=25)
-    type_line = models.CharField(max_length=254)
-    oracle_text = models.CharField(max_length=254)
-    power = models.CharField(max_length=5)
-    toughness = models.CharField(max_length=5)
-    colors = ListCharField(encoder=None, decoder=None)
-    color_identity =
-    mtg_set = models.ForeignKey('Mtg_Sets', null=False, blank=False)
-    rarity =
-    collector_number =
-    price =
-    sales_category = models.ForeignKey('Sales_Category', null=False, blank=False)
-    artist =
-    image_url =
-    image =
+    lang = models.CharField(max_length=32, blank=True)
+    type_line = models.CharField(max_length=254, blank=True)
+    oracle_text = models.CharField(max_length=254, blank=True)
+    power = models.CharField(max_length=8, blank=True)
+    toughness = models.CharField(max_length=8, blank=True)
+    colors = models.JSONField(default=list)
+    color_identity = models.JSONField(default=list)
+    mtg_set = models.ForeignKey('Mtg_Sets', null=True, blank=True, on_delete=models.SET_NULL)
+    rarity = models.CharField(max_length=8, blank=True)
+    collector_number = models.CharField(max_length=8, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    sales_category = models.ForeignKey('Sales_Category', null=True, blank=True, on_delete=models.SET_NULL)
+    artist = models.CharField(max_length=32, blank=True)
+    image_url = models.URLField(max_length=1024, blank=True)
+    image = models.ImageField(blank=True)
+
+    def __str__(self):
+        return self.name
