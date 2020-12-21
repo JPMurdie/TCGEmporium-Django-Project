addc22f1417deb15ed96f665a77f93daf61500b3
JPMurdie <jpmurdie@gmail.com>
2020-12-21 08:27:46 +0000

    altered mtg_cards data to show Power/Toughness as strings. Created Sales Cat & Mtg Set models and partly completed Mtg Cards model.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+class Sales_Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Mtg_Sets(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Mtg_Cards(models.Model):
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    lang = models.CharField(max_length=25)
+    type_line = models.CharField(max_length=254)
+    oracle_text = models.CharField(max_length=254)
+    power = models.CharField(max_length=5)
+    toughness = models.CharField(max_length=5)
+    colors = ListCharField(encoder=None, decoder=None)
+    color_identity =
+    mtg_set = models.ForeignKey('Mtg_Sets', null=False, blank=False)
+    rarity =
+    collector_number =
+    price =
+    sales_category = models.ForeignKey('Sales_Category', null=False, blank=False)
+    artist =
+    image_url =
+    image =
