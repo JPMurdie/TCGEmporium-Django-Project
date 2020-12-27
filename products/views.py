876eb6fc0cd1af2dbfecba5035b2383d6a4abbe1
JPMurdie <jpmurdie@gmail.com>
2020-12-27 18:47:14 +0000

    Added product details page from BA as template

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Mtg_Cards
 
 
@@ -12,3 +12,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Mtg_Cards, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
