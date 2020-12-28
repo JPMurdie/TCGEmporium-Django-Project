190026a52f506e6959f27e6ffb29c3e242639e89
JPMurdie <jpmurdie@gmail.com>
2020-12-28 17:49:14 +0000

    Added functionality for search bar.

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Mtg_Cards
 
 
@@ -6,9 +8,21 @@ def all_products(request):
     """ A view to show all products including sort and search queries"""
 
     products = Mtg_Cards.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(oracle_text__icontains=query) | Q(mtg_set__name__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
